a7857015d07443cd02e218cfd67ab0da9d49ca35
RoryXChen <rory.chen.xy@gmail.com>
2024-06-12 18:02:52 +1000

    deleted unused test files

---
+++ datarepexp/app.R
@@ -0,0 +1,9 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+
+pkgload::load_all()
+
+options("golem.app.prod" = TRUE)
+
+datarepexp::run_app() # add parameters here (if any)
